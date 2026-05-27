4bfba29271e1bb6ab756488e068a27c76f8ca510
crimson-planks <koreaminecraftmonster@gmail.com>
2026-05-27 22:29:23 +0900

    Initial commit

---
+++ src/components/NumberDisplay.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { formatNumber, type NumberNotationWithOptions } from '@/number_notation';
+import { computed } from 'vue';
+
+const props = defineProps<{ value: number; notation: NumberNotationWithOptions }>()
+const formattedValue = computed(() => formatNumber(props.value, props.notation));
+</script>
+<template>
+  {{ formattedValue }}
+</template>
